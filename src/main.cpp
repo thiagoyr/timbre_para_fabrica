7512f0930db51281f25fab36876366c8cf52ef2d
Thiago Yair  Reller <ThiagoR@exo.com.ar>
2022-06-10 11:20:28 -0300

    Testeo de funciones alarma + pepe

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,56 @@
 #include <Arduino.h>
 
-#define on_value_alarms 0
+#define on_value_alarms 1
+#define alarm1 3
+#define alarm2 4
+#define alarm3 5
+#define alarm4 6
+#define duration_of_alarm 500
 
-void setup() {
-  
+void set_one_alarm_on(int pin_alarm1, int duration_alarms);
+void set_two_alarm_on(int pin_alarma1, int pin_alarma2, int duration_alarms);
+void set_four_alarm_on(int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_alarma4, int duration_alarms);
+
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(alarm1,OUTPUT);
+  pinMode(alarm2,OUTPUT);
+  pinMode(alarm3,OUTPUT);
+  pinMode(alarm4,OUTPUT);
 }
 
+void loop(){
+    
+    set_one_alarm_on(duration_of_alarm, alarm1);
+    set_one_alarm_on(duration_of_alarm, alarm2);
+    set_one_alarm_on(duration_of_alarm, alarm3);
+    set_one_alarm_on(duration_of_alarm, alarm4);
 
+    set_two_alarm_on(duration_of_alarm, alarm1, alarm2);
+    set_two_alarm_on(duration_of_alarm, alarm3, alarm4);
 
-void loop() {
-
+    set_four_alarm_on(duration_of_alarm, alarm1, alarm2, alarm3, alarm4);
 }
 
-
-void set_one_alarm_on(int pin_alarm, int duration_alarms){
-  digitalWrite(pin_alarm, on_value_alarms);
+// FUNCIONES
+void set_one_alarm_on(int duration_alarms, int pin_alarm1){
+  digitalWrite(pin_alarm1, on_value_alarms);
   delay(duration_alarms);
-  digitalWrite(pin_alarm, !on_value_alarms);
+  digitalWrite(pin_alarm1, !on_value_alarms);
+  delay(duration_alarms);
 }
 
-void set_two_alarm_on(int pin_alarma1, int pin_alarma2, int duration_alarms){
+void set_two_alarm_on(int duration_alarms, int pin_alarma1, int pin_alarma2){
   digitalWrite(pin_alarma1, on_value_alarms);
   digitalWrite(pin_alarma2, on_value_alarms);
   delay(duration_alarms);
   digitalWrite(pin_alarma1, !on_value_alarms);
   digitalWrite(pin_alarma2, !on_value_alarms);
+  delay(duration_alarms);
 }
 
-void set_four_alarm_on(int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_alarma4, int duration_alarms){
+void set_four_alarm_on(int duration_alarms, int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_alarma4){
   digitalWrite(pin_alarma1, on_value_alarms);
   digitalWrite(pin_alarma2, on_value_alarms);
   digitalWrite(pin_alarma3, on_value_alarms);
@@ -37,3 +60,5 @@ void set_four_alarm_on(int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_a
   digitalWrite(pin_alarma2, !on_value_alarms);
   digitalWrite(pin_alarma3, !on_value_alarms);
   digitalWrite(pin_alarma4, !on_value_alarms);
+  delay(duration_alarms);
+  }
---
+++ test/pepe.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+#define on_value_alarms 0
+
+void setup() {
+  
+}
+
+
+
+void loop() {
+
+}
+
+
+void set_one_alarm_on(int pin_alarm, int duration_alarms){
+  digitalWrite(pin_alarm, on_value_alarms);
+  delay(duration_alarms);
+  digitalWrite(pin_alarm, !on_value_alarms);
+}
+
+void set_two_alarm_on(int pin_alarma1, int pin_alarma2, int duration_alarms){
+  digitalWrite(pin_alarma1, on_value_alarms);
+  digitalWrite(pin_alarma2, on_value_alarms);
+  delay(duration_alarms);
+  digitalWrite(pin_alarma1, !on_value_alarms);
+  digitalWrite(pin_alarma2, !on_value_alarms);
+}
+
+void set_four_alarm_on(int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_alarma4, int duration_alarms){
+  digitalWrite(pin_alarma1, on_value_alarms);
+  digitalWrite(pin_alarma2, on_value_alarms);
+  digitalWrite(pin_alarma3, on_value_alarms);
+  digitalWrite(pin_alarma4, on_value_alarms);
+  delay(duration_alarms);
+  digitalWrite(pin_alarma1, !on_value_alarms);
+  digitalWrite(pin_alarma2, !on_value_alarms);
+  digitalWrite(pin_alarma3, !on_value_alarms);
+  digitalWrite(pin_alarma4, !on_value_alarms);
