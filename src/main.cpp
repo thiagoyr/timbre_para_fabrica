12398c9d9c7387c9ca00a2df9445180da3d201cd
Thiago Yair  Reller <ThiagoR@exo.com.ar>
2022-06-15 10:31:22 -0300

    Funciones para prender y elegir alarmas funcionando correctamente

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,11 @@
 #define alarm4 6
 #define duration_of_alarm 500
 
-void set_one_alarm_on(int pin_alarm1, int duration_alarms);
-void set_two_alarm_on(int pin_alarma1, int pin_alarma2, int duration_alarms);
-void set_four_alarm_on(int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_alarma4, int duration_alarms);
+bool alarms_states[] = {false,false,false,false};
+
+void set_alarm(int duration_alarms, bool alarms[]);
+void choose_alarms(int alarm_number);
+
 
 
 void setup(){
@@ -21,44 +23,37 @@ void setup(){
 }
 
 void loop(){
-    
-    set_one_alarm_on(duration_of_alarm, alarm1);
-    set_one_alarm_on(duration_of_alarm, alarm2);
-    set_one_alarm_on(duration_of_alarm, alarm3);
-    set_one_alarm_on(duration_of_alarm, alarm4);
-
-    set_two_alarm_on(duration_of_alarm, alarm1, alarm2);
-    set_two_alarm_on(duration_of_alarm, alarm3, alarm4);
-
-    set_four_alarm_on(duration_of_alarm, alarm1, alarm2, alarm3, alarm4);
+  
+  choose_alarms(2);
+  choose_alarms(3);
+  set_alarm(duration_of_alarm, alarms_states);
+  choose_alarms(4);
+  set_alarm(duration_of_alarm, alarms_states);
 }
 
 // FUNCIONES
-void set_one_alarm_on(int duration_alarms, int pin_alarm1){
-  digitalWrite(pin_alarm1, on_value_alarms);
+void set_alarm(int duration_alarms, bool alarms_enabled[]){
+  digitalWrite(alarm1, alarms_enabled[0]);
+  digitalWrite(alarm2, alarms_enabled[1]);
+  digitalWrite(alarm3, alarms_enabled[2]);
+  digitalWrite(alarm4, alarms_enabled[3]);
   delay(duration_alarms);
-  digitalWrite(pin_alarm1, !on_value_alarms);
+  digitalWrite(alarm1,false);
+  digitalWrite(alarm2,false);
+  digitalWrite(alarm3,false);
+  digitalWrite(alarm4,false);
   delay(duration_alarms);
-}
-
-void set_two_alarm_on(int duration_alarms, int pin_alarma1, int pin_alarma2){
-  digitalWrite(pin_alarma1, on_value_alarms);
-  digitalWrite(pin_alarma2, on_value_alarms);
-  delay(duration_alarms);
-  digitalWrite(pin_alarma1, !on_value_alarms);
-  digitalWrite(pin_alarma2, !on_value_alarms);
-  delay(duration_alarms);
-}
-
-void set_four_alarm_on(int duration_alarms, int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_alarma4){
-  digitalWrite(pin_alarma1, on_value_alarms);
-  digitalWrite(pin_alarma2, on_value_alarms);
-  digitalWrite(pin_alarma3, on_value_alarms);
-  digitalWrite(pin_alarma4, on_value_alarms);
-  delay(duration_alarms);
-  digitalWrite(pin_alarma1, !on_value_alarms);
-  digitalWrite(pin_alarma2, !on_value_alarms);
-  digitalWrite(pin_alarma3, !on_value_alarms);
-  digitalWrite(pin_alarma4, !on_value_alarms);
-  delay(duration_alarms);
+  for(int i=0; i<=3;i++){
+    alarms_states[i] = false;
   }
+  }
+
+  void choose_alarms(int alarm_number){
+    if( 1<=alarm_number && alarm_number<=4 ){
+      alarms_states[alarm_number-1]=true;
+    }else{
+      Serial.println("The alarm number does not exist");
+    }
+    
+  }
+
--- test/pepe.cpp
+++ test/pepe.cpp
@@ -1,4 +1,70 @@
 #include <Arduino.h>
+#include <Arduino.h>
+
+#define on_value_alarms 1
+#define alarm1 3
+#define alarm2 4
+#define alarm3 5
+#define alarm4 6
+#define duration_of_alarm 500
+#define first_alarm_ring 15
+#define second_alarm_ring 14
+#define third_alarm_ring 13
+
+bool alarms_states[] = {false,false,false,false};
+
+void set_alarm(int duration_alarms, bool alarms[]);
+int choose_alarms(int alarm_number, bool alarm_state);
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(alarm1,OUTPUT);
+  pinMode(alarm2,OUTPUT);
+  pinMode(alarm3,OUTPUT);
+  pinMode(alarm4,OUTPUT);
+}
+
+void loop(){
+  choose_alarms(2);
+  set_alarm(duration_of_alarm, alarms_states);
+  choose_alarms(4);
+  set_alarm(duration_of_alarm, alarms_states);
+}
+
+// FUNCIONES
+void set_alarm(int duration_alarms, bool alarms_enabled[]){
+  digitalWrite(alarm1, alarms_enabled[0]);
+  digitalWrite(alarm2, alarms_enabled[1]);
+  digitalWrite(alarm3, alarms_enabled[2]);
+  digitalWrite(alarm4, alarms_enabled[3]);
+  delay(duration_alarms);
+  digitalWrite(alarm1,false);
+  digitalWrite(alarm2,false);
+  digitalWrite(alarm3,false);
+  digitalWrite(alarm4,false);
+  delay(duration_alarms);
+  for(int i=0; i<=3;i++){
+    alarms_states[i] = false;
+    }
+  }
+
+  int choose_alarms(int alarm_number){
+    if( 1<=alarm_number && alarm_number <=4 ){
+      alarms_states[alarm_number-1]=true;
+      
+    }else{
+      Serial.println("The alarm number does not exist");
+    }
+  }
+
+
+
+
+
+
+
+
+
 
 #define on_value_alarms 0
 
