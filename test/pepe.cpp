7a12286eb45b08aebdfaedc42eaea80bd785f666
Thiago Yair  Reller <ThiagoR@exo.com.ar>
2022-06-24 11:42:11 -0300

    Working Code, can be used to Merge

--- test/pepe.cpp
+++ test/pepe.cpp
@@ -1,105 +1,69 @@
-#include <Arduino.h>
+//Libraries
 #include <Arduino.h>
 
-#define on_value_alarms 1
-#define alarm1 3
+//Defines
+#define on_value_alarms 1 //value used to turn on alarms
+#define alarm1 3 //alarm pins
 #define alarm2 4
 #define alarm3 5
 #define alarm4 6
 #define duration_of_alarm 500
-#define first_alarm_ring 15
-#define second_alarm_ring 14
-#define third_alarm_ring 13
+#define number_of_alarms 4 //number of alarms used
 
-bool alarms_states[] = {false,false,false,false};
+//Global Variables
+bool alarms_states[number_of_alarms] = {false,false,false,false};
+int alarm_pin[number_of_alarms] = {alarm1, alarm2, alarm3, alarm4};
 
-void set_alarm(int duration_alarms, bool alarms[]);
-int choose_alarms(int alarm_number, bool alarm_state);
+//Functions
+void set_enabled_alarms(int duration_alarms, bool alarms[]);
+void choose_alarm(int alarm_number);
+void choose_alarms2(int alarm_number1, int alarm_number2);
+void choose_alarms4(int alarm_number1, int alarm_number2, int alarm_number3, int alarm_number4);
+
 
 void setup(){
   Serial.begin(9600);
-  pinMode(alarm1,OUTPUT);
-  pinMode(alarm2,OUTPUT);
-  pinMode(alarm3,OUTPUT);
-  pinMode(alarm4,OUTPUT);
+  for(int i=0; i<number_of_alarms;i++){
+    pinMode(alarm_pin[i],OUTPUT);
+  }
 }
 
 void loop(){
-  choose_alarms(2);
-  set_alarm(duration_of_alarm, alarms_states);
-  choose_alarms(4);
-  set_alarm(duration_of_alarm, alarms_states);
+  
 }
 
 // FUNCIONES
-void set_alarm(int duration_alarms, bool alarms_enabled[]){
-  digitalWrite(alarm1, alarms_enabled[0]);
-  digitalWrite(alarm2, alarms_enabled[1]);
-  digitalWrite(alarm3, alarms_enabled[2]);
-  digitalWrite(alarm4, alarms_enabled[3]);
+/*Enables alarms. First parameter uses a delay (in milliseconds) to determine how long they will ring, and must use a global bool vector called "alarm_states" as a second parameter to ensure working of the function. Sets all positions of global vector to 0 when finished. Has a delay at the end because it may be needed when using Leds.*/
+void set_enabled_alarms(int duration_alarms, bool alarms_enabled[]){
+  for(int i=0;i<number_of_alarms;i++){
+    digitalWrite(alarm_pin[i], alarms_enabled[i]);
+  }
   delay(duration_alarms);
-  digitalWrite(alarm1,false);
-  digitalWrite(alarm2,false);
-  digitalWrite(alarm3,false);
-  digitalWrite(alarm4,false);
-  delay(duration_alarms);
-  for(int i=0; i<=3;i++){
+  for(int i=0; i< number_of_alarms;i++){
     alarms_states[i] = false;
-    }
+  }
+  delay(duration_alarms)
   }
 
-  int choose_alarms(int alarm_number){
-    if( 1<=alarm_number && alarm_number <=4 ){
+  /*Chooses which alarms to enable in a global bool vector called "alarm_states". Number of alarms starts at 1, and if the number isn't in the defined range of alarms it prints that the desired alarm does not exist.*/
+  void choose_alarm(int alarm_number){
+    if( 1<=alarm_number && alarm_number<=number_of_alarms ){
       alarms_states[alarm_number-1]=true;
-      
     }else{
       Serial.println("The alarm number does not exist");
     }
   }
 
+  /*Chooses 2 alarms at the same time. Must have 2 parameters, the position of both alarms in the alarms_states vector.*/
+  void choose_alarms2(int alarm_number1, int alarm_number2){
+    choose_alarm(alarm_number1);
+    choose_alarm(alarm_number2);
+  }
 
-
-
-
-
-
-
-
-
-#define on_value_alarms 0
-
-void setup() {
-  
-}
-
-
-
-void loop() {
-
-}
-
-
-void set_one_alarm_on(int pin_alarm, int duration_alarms){
-  digitalWrite(pin_alarm, on_value_alarms);
-  delay(duration_alarms);
-  digitalWrite(pin_alarm, !on_value_alarms);
-}
-
-void set_two_alarm_on(int pin_alarma1, int pin_alarma2, int duration_alarms){
-  digitalWrite(pin_alarma1, on_value_alarms);
-  digitalWrite(pin_alarma2, on_value_alarms);
-  delay(duration_alarms);
-  digitalWrite(pin_alarma1, !on_value_alarms);
-  digitalWrite(pin_alarma2, !on_value_alarms);
-}
-
-void set_four_alarm_on(int pin_alarma1,int pin_alarma2,int pin_alarma3,int pin_alarma4, int duration_alarms){
-  digitalWrite(pin_alarma1, on_value_alarms);
-  digitalWrite(pin_alarma2, on_value_alarms);
-  digitalWrite(pin_alarma3, on_value_alarms);
-  digitalWrite(pin_alarma4, on_value_alarms);
-  delay(duration_alarms);
-  digitalWrite(pin_alarma1, !on_value_alarms);
-  digitalWrite(pin_alarma2, !on_value_alarms);
-  digitalWrite(pin_alarma3, !on_value_alarms);
-  digitalWrite(pin_alarma4, !on_value_alarms);
+  /*Chooses 4 alarms at the same time. Must have 4 parameters, the position of the alarms in the alarms_states vector.*/
+  void choose_alarms4(int alarm_number1, int alarm_number2,int alarm_number3, int alarm_number4){
+    choose_alarm(alarm_number1);
+    choose_alarm(alarm_number2);
+    choose_alarm(alarm_number3);
+    choose_alarm(alarm_number4);
+  }
